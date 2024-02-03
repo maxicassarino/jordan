54da649ed910e76ceb224957544746c41ad4cfd5
maxicassarino <maximocassarino@gmail.com>
2024-02-03 17:57:37 -0300

    Version 4.0

--- src/Components/Contenedor/Lista/Detalle/Contador/Contador.jsx
+++ src/Components/Contenedor/Lista/Detalle/Contador/Contador.jsx
@@ -5,7 +5,13 @@ import { DarkContext } from "../../../../../App";
 
 function Contador({zapatilla, stock}) {
 
-    const { agregarAlCarrito } = useContext(DarkContext);
+    const { carrito, setCarrito, darkMode } = useContext(DarkContext);
+
+    const styles = {
+        boton: {
+            borderColor: darkMode ? "grey" : "black",
+        }
+    };
 
     // Count
 
@@ -13,7 +19,34 @@ function Contador({zapatilla, stock}) {
 
     const [Count, setCount] = useState(1);
 
-    const [isAdded, setIsAdded] = useState(false);
+    const [botonListo, setBotonListo] = useState(false);
+
+
+    const handleAgregarAlCarrito = () => {
+        const productoEnCarrito = carrito.find((producto) => producto.id === zapatilla.id);
+    
+        if (productoEnCarrito) {
+          // Si el producto ya está en el carrito, actualiza la cantidad
+            const nuevoCarrito = carrito.map((producto) =>
+                producto.id === zapatilla.id ? { ...producto, cantidad: producto.cantidad + Count } : producto
+            );
+            setCarrito(nuevoCarrito);
+        } else {
+          // Si el producto no está en el carrito, agrégalo
+            setCarrito([...carrito, { ...zapatilla, cantidad: Count }]);
+        }
+    
+        // Reinicia la cantidad a 1 después de agregar al carrito
+        setCount(1);
+
+        
+        // Después de 5 segundos, vuelve a "Agregar"
+        setBotonListo(true);
+        setTimeout(() => {
+            setBotonListo(false);
+        }, 1000);
+    };
+
 
     function aumentarCount() {
         if (Count < maxCount) {
@@ -27,19 +60,15 @@ function Contador({zapatilla, stock}) {
         }
     } 
 
-    const handleAgregarAlCarrito = () => {
-        agregarAlCarrito({ ...zapatilla, cantidad: Count });
-        setIsAdded(true);
-    };
 
     return (
         <div className="carrito">
             <div className="contador">
-                <button className="botonesContador" onClick={restarCount}>-</button>
+                <button className="botonesContador" onClick={restarCount} style={styles.boton}>-</button>
                 <span id="Numero">{Count}</span>
-                <button className="botonesContador" onClick={aumentarCount}>+</button>
+                <button className="botonesContador" onClick={aumentarCount} style={styles.boton}>+</button>
             </div>
-            <button className="botonesContador" id="agregar" onClick={handleAgregarAlCarrito} disabled={isAdded}>{isAdded ? "Listo" : "Agregar"}</button>
+            <button className="botonesContador" id="agregar" onClick={handleAgregarAlCarrito} disabled={botonListo} style={styles.boton}>{botonListo ? "Listo" : "Agregar"}</button>
         </div>
 );
 }
